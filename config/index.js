4d58f3d19991fcdfd84569859fe081004e653720
Clement Nduonyi <cndigitech@gmail.com>
2022-08-10 12:30:30 +0100

    fix bug in config index file

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
-if(process.env.NODE_ENV === 'prodection'){
-    module.exports = require('./prodection');
+if(process.env.NODE_ENV === 'production'){
+    module.exports = require('./production');
 }else{
     module.exports = require('./dev')
 }
